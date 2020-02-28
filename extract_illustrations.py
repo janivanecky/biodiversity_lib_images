0241acaa99a299700de8de64d5fb5ee5e680da93
Jan Ivanecky <jan.ivanecky@hotmail.com>
2020-02-27 23:32:38 -0800

    Better filtering of illustration pages

--- extract_illustrations.py
+++ extract_illustrations.py
@@ -1,26 +1,36 @@
 import argparse
 
 def extract_illustrations(page_file, output_file):
-    # Read page ids for all the illustrations.
-    illustration_ids = []
+    # Read page ids for all the illustrations and the other pages.
+    illustration_ids = set()
+    non_illustration_ids = set()
+
     with open(page_file, encoding='utf8') as f:
-        l = next(f)  # Skip the table header.
+        next(f)  # Skip the table header.
         for line in f:
             line = line.split('\t')
-            page_type, page_id = line[8], line[0]
-            if page_type != 'Illustration':
-                continue
-            illustration_ids.append(page_id)
-
-    # Remove duplicates and sort.
-    illustration_ids = sorted(list(set(illustration_ids)))
+            page_type, page_id, page_prefix = line[8], line[0], line[6]
+            is_correct_page_type = page_type == 'Drawing' or page_type == 'Illustration'
+            is_correct_prefix = (
+                'plate' in page_prefix.lower()
+                or 'fig' in page_prefix.lower()
+                or 'pl.' in page_prefix.lower()
+                or 'pl ' in page_prefix.lower()
+            )
+            if is_correct_page_type and is_correct_prefix:
+                illustration_ids.add(page_id)
+            else:
+                non_illustration_ids.add(page_id)
+    # We want to keep only page which are exclusively illustrations.
+    illustration_ids = illustration_ids - non_illustration_ids
+    illustration_ids = sorted(list(illustration_ids))
 
     # Write to output file.
     with open(output_file, 'w') as f:
         f.write('\n'.join(illustration_ids))
 
     # Print status.
-    print("Saved {} illustration ids to {}.".format(len(illustration_ids), output_file))
+    print("Saved {} page ids to {}.".format(len(illustration_ids), output_file))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract illustration ids from page file.')
